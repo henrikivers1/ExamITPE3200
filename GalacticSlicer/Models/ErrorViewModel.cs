1ffa89617f3356d41ef4cab458770351d4e4a104
JimPower1711 <endernight989@gmail.com>
2026-09-17 12:14:33 +0200

    Standard MVC + TODO liste

---
+++ GalacticSlicer/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GalacticSlicer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
