9fe3b7ba06cf40fb2c5ade1e08636b5e8c0454ec
Henrik Iversen <113095290+L3GNDZ@users.noreply.github.com>
2026-09-19 16:26:27 +0200

    Add Challenge CRUD functionality

--- GalacticSlicer/Controllers/ChallengerController.cs
+++ GalacticSlicer/Controllers/ChallengerController.cs
@@ -14,27 +14,100 @@ namespace ExamITPE3200.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var challenges = _context.Challenges.ToList();
+
+            return View(challenges);
         }
 
         public IActionResult Details(int id)
         {
-            return View();
+            var challenge = _context.Challenges.FirstOrDefault(c => c.ChallengeId == id);
+
+            if (challenge == null)
+            {
+                return NotFound();
+            }
+
+            return View(challenge);
         }
 
         public IActionResult Create()
         {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Challenge challenge)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Challenges.Add(challenge);
+                _context.SaveChanges();
 
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(challenge);
+        }
         public IActionResult Edit(int id)
         {
-            return View();
+            var challenge = _context.Challenges.Find(id);
+
+            if (challenge == null)
+            {
+                return NotFound();
+            }
+
+            return View(challenge);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Challenge challenge)
+        {
+            if (id != challenge.ChallengeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Challenges.Update(challenge);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(challenge);
         }
 
         public IActionResult Delete(int id)
         {
-            return View();
+            var challenge = _context.Challenges.Find(id);
+
+            if (challenge == null)
+            {
+                return NotFound();
+            }
+
+            return View(challenge);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var challenge = _context.Challenges.Find(id);
+
+            if (challenge == null)
+            {
+                return NotFound();
+            }
+
+            _context.Challenges.Remove(challenge);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
